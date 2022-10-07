7892d2fda0d5ee1e21c78f3c98579fb5979dc06d
Imran Khan <imran@accupara.com>
2022-10-07 18:29:11 +0530

    - remove leading and trailing whitespaces while parsing output.

--- utils.go
+++ utils.go
@@ -61,7 +61,7 @@ func (c *command) Run(arg ...string) ([][]string, error) {
 	output := make([][]string, len(lines))
 
 	for i, l := range lines {
-		output[i] = strings.Split(l, "\t")
+		output[i] = strings.Split(strings.TrimSpace(l), "\t")
 	}
 
 	return output, nil
